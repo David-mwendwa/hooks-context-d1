fe21b11c5bf2b35ca457f85c2539a5e3eb0c55a3
techdave <klatzdavie19@gmail.com>
2022-04-06 10:52:33 +0300

    make context dynamic

--- src/App.js
+++ src/App.js
@@ -28,8 +28,9 @@ function App() {
   };
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn: isLoggedIn }}>
-      <MainHeader onLogout={logoutHandler} />
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, onLogout: logoutHandler }}>
+      <MainHeader />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
